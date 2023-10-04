4241cdc5255b818102495867550c90bf3c186c40
Xia Webster <xmw2003@gmail.com>
2023-10-04 13:25:44 -0700

    getting users by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,7 +41,25 @@ const users = {
        }
     ]
  }
- 
- app.get('/users', (req, res) => {
+
+
+const findUserByName = (name) => { 
+    return users['users_list']
+        .filter( (user) => user['name'] === name); 
+}
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+});
+
+app.get('/users', (req, res) => {
     res.send(users);
 });
